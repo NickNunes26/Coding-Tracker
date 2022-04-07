7232a022371034ba2ea50ed0b9a926f220df05b2
Nickollas Nunes <nickollas_nunes@hotmail.com>
2022-04-07 08:55:39 +0200

    Created classes, added command to create table. Pending sending this command to a separate class.

--- Coding-Tracker/Program.cs
+++ Coding-Tracker/Program.cs
@@ -1,2 +1,46 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using ConsoleTableExt;
+using Microsoft.Data.Sqlite;
+
+/*
+ EXAMPLE OF HOW THE TABLE THING SHOULD WORK. TO DELETE LATER.
+var tableData = new List<List<object>>
+{
+    new List<object>{ "Sakura Yamamoto", "Support Engineer", "London", 46},
+    new List<object>{ "Serge Baldwin", "Data Coordinator", "San Francisco", 28, "something else" },
+    new List<object>{ "Shad Decker", "Regional Director", "Edinburgh"},
+};
+
+ConsoleTableBuilder
+    .From(tableData)
+    .ExportAndWriteLine();
+*/
+
+string connectionString = @"Data Source = Coding-Tracker.db";
+
+using (var connection = new SqliteConnection(connectionString))
+{
+
+    using (var tableCmd = connection.CreateCommand())
+    {
+
+        tableCmd.CommandText =
+            @"CREATE TABLE IF NOT EXISTS Coding-Tracker (
+            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            StartTime TEXT
+            EndTime TEXT
+            Duration REAL
+            )";
+
+        connection.Open();
+
+        tableCmd.ExecuteNonQuery();
+
+        connection.Close();
+
+        
+
+    }
+
+
+}
